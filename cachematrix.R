a5422d17189e5c52fcd50c3847e3e7cfbf42866c
grahampayette <graham@payetteville.com>
2015-11-22 16:03:12 -0800

    Revert "Week 3 assignment"
    This reverts commit 4638a8bc7493ab68d7b7c2c124c23dac3d442396.

--- cachematrix.R
+++ cachematrix.R
@@ -2,34 +2,9 @@
 ## functions do
 
 ## Write a short comment describing this function
-## initializes set/get functions for a square matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-  ## initialize variables
-  
-  tmp <- NULL
-  
-  ## test for type
-  if (!is.matrix(x)) {
-    warning("argument is not a matrix: returning NA")
-    return(NA_real_)
-  }
-  
-  ## set value
-  set <- function(y) {
-    x <<- y
-    tmp <<- NULL
-  }  
-  
-  ## get value
-  get <- function() x 
-  ## get/set stored values
-  setinv <- function(solve) tmp <<- solve
-  getinv <- function() tmp 
-  list(set = set, get = get,
-       setinv = setinv,
-       getinv = getinv)    
-  
+
 }
 
 
@@ -37,15 +12,4 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  tmp <- x$getinv()
-  
-  if(!is.null(tmp)) {
-    message("getting cached data")
-    return(tmp)
-  }
-  data <- x$get()
-  tmp <- solve(data, ...)
-  x$setinv(tmp)
-  tmp 
-  
 }
